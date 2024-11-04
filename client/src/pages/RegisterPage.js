8a3d378aeb9de0c02ecc169187c8fe2dd9c43ca6
Udaydeep Singh Dhir <57069443+udaydeep28@users.noreply.github.com>
2024-11-04 19:24:34 +0530

    Update RegisterPage.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -12,7 +12,7 @@ export default function RegisterPage() {
   async function register(ev) {
     try {
       ev.preventDefault();
-      const response = await fetch("http://localhost:4000/register", {
+      const response = await fetch("https://posthub-backend.onrender.com/register", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
