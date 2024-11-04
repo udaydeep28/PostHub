f5460b9291215aaa90da8cd5a537187dc282f500
udaydeep28 <udaydeepsinghd@gmail.com>
2024-11-04 17:52:55 +0530

    first commit

---
+++ client/src/partials/Footer.js
@@ -0,0 +1,25 @@
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-content">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li>
+          <a href="/login">Account</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <p className="developer-credits">
+      Developed by: <a href="https://github.com/udaydeep28">PostHub</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
