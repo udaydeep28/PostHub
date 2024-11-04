c15539b7ffc178b1c08ab764355543be2653c57f
Udaydeep Singh Dhir <57069443+udaydeep28@users.noreply.github.com>
2024-11-04 19:22:08 +0530

    Update BlogsPage.js
    update backend link

--- client/src/pages/BlogsPage.js
+++ client/src/pages/BlogsPage.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 export default function BlogsPage() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:4000/post").then((response) => {
+    fetch("https://posthub-backend.onrender.com/post").then((response) => {
       response.json().then((posts) => {
         setPosts(posts);
       });
